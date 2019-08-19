2351eae9c45c7f1cdb31e614f1518d9f87a58426
zhangyiran01 <54269459+zhangyiran01@users.noreply.github.com>
2019-08-19 20:43:30 +0800

    Update practice-1.js

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    var result = [];
+    var map = new Map();
+    for (var i=0;i<collection.length;i++){
+        if (map.get(collection[i])==null){
+            map.set(collection[i],1);
+        }else{
+            map.set(collection[i],map.get(collection[i])+1);
+        }
+    }
+    map.forEach(function(key,value){
+        result.push({key:value,count:key})
+    });
+    return result;
 }
