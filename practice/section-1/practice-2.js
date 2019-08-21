8f4f777a601ef788bb573774fa61455948e9d9a0
zhangyiran01 <54269459+zhangyiran01@users.noreply.github.com>
2019-08-21 11:53:16 +0800

    Update practice-2.js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,19 +1,18 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
- var c = new Array();
-  collectionB=collectionB[0];
+  var c = new Array();
+  let collectionC=collectionB[0]
   for(var i = 0; i<collectionA.length ; i++)
   {
-    for(var j = 0; j<collectionB.length ; j++)
+    for(var j = 0; j<collectionC.length ; j++)
     {
-      if (collectionA[i] == collectionB[j])
+      if (collectionA[i] == collectionC[j])
       {
         c.push(collectionA[i]);
-        break;
+          break;
       }
     }
   }
-  return c;
-}
-
+    return c;
+  }
